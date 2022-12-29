f7440a3f427c8392125f28a53675aafc791e926e
ruilinqi <ruilinqi7@gmail.com>
2022-12-29 23:18:29 +0000

    Edited tables so tables and seeds are matched

--- db/schema/02_fruits.sql
+++ db/schema/02_fruits.sql
@@ -1,11 +1,11 @@
-DROP TABLE IF EXISTS products CASCADE;
-CREATE TABLE products (
+DROP TABLE IF EXISTS fruits CASCADE;
+CREATE TABLE fruits (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   price FLOAT,
   description TEXT,
-  product_picture_url VARCHAR(255),
+  fruit_picture_url VARCHAR(255),
   owner_id INTEGER REFERENCES users(id),
   isSold BOOLEAN DEFAULT FALSE,
-  listTime DATE
+  list_time DATE
 );
--- db/seeds/02_fruits_seed.sql
+++ db/seeds/02_fruits_seed.sql
@@ -1,5 +1,4 @@
--- Items table seeds here (Example)
-INSERT INTO products (name, product_picture_url, description, price, owner_id, isSold, list_time)
+INSERT INTO fruits (id, name, description, fruit_picture_url, price, owner_id, isSold, list_time)
 VALUES (1, 'apple', 'organic, each', 'https://images.unsplash.com/photo-1610832958506-aa56368176cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80', 1.25, 3, FALSE, '2022-12-27'),
 (2, 'banana', 'organic, bunch', 'https://images.unsplash.com/photo-1610832958506-aa56368176cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80', 0.99, 3, FALSE, '2022-12-27'),
 (3, 'cherry', 'organic, 1LB', 'https://images.unsplash.com/photo-1610832958506-aa56368176cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80', 4.99, 3, FALSE, '2022-12-27'),
