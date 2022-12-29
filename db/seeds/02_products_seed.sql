c74462a668e1c9eb313b05eb89f3f335141b8fc4
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2022-12-28 23:05:36 -0500

    Set products seed

---
+++ db/seeds/02_products_seed.sql
@@ -0,0 +1,6 @@
+-- Items table seeds here (Example)
+INSERT INTO products (name, product_picture_url, description, price, owner_id, isSold, list_time)
+VALUES (1, 'apple', 'organic, each', 'https://images.unsplash.com/photo-1610832958506-aa56368176cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80', 1.25, 3, FALSE, '2022-12-27'),
+(2, 'banana', 'organic, bunch', 'https://images.unsplash.com/photo-1610832958506-aa56368176cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80', 0.99, 3, FALSE, '2022-12-27'),
+(3, 'cherry', 'organic, 1LB', 'https://images.unsplash.com/photo-1610832958506-aa56368176cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80', 4.99, 3, FALSE, '2022-12-27'),
+(4, 'durian', 'organic, each', 'https://images.unsplash.com/photo-1610832958506-aa56368176cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80', 14.99, 3, FALSE, '2022-12-27');
