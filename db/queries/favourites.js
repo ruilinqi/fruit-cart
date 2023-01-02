99003b8cf99f2407f1f75e74d98e5762610d59b8
ruilinqi <ruilinqi7@gmail.com>
2023-01-02 21:04:22 +0000

    Fixed the issue of favourites

--- db/queries/favourites.js
+++ db/queries/favourites.js
@@ -1,7 +1,7 @@
 const db = require('../connection');
 
 const getFavourites = () => {
-  return db.query(`SELECT * FROM favourites
+  return db.query(`SELECT fruits.id, fruits.name as fruit_name, fruits.price, users.id, users.name as user_name FROM favourites
   JOIN fruits ON fruits.id = favourites.fruit_id
   JOIN users ON users.id = favourites.user_id
   WHERE users.id = 1;`)
