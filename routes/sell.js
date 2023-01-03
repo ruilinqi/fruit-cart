e831a8cdf6fc9e29f43b92d5532522147648da38
ruilinqi <ruilinqi7@gmail.com>
2023-01-03 16:10:43 +0000

    Fixed issues of sell function

--- routes/sell.js
+++ routes/sell.js
@@ -13,15 +13,17 @@ router.post("/sell", (req, res) => {
   const description = req.body.description;
   const imageURL = req.body.fruit_picture_url;
   const price = req.body.price;
-  const ownerId = req.body.owner_id;
+  const ownerID = 3;
+  // const ownerID = req.session.owner_id;
   const isSold = false;
   const listTime = req.body.list_time;
 
   const sql = `INSERT INTO fruits (id, name, description, fruit_picture_url, price, owner_id, isSold, list_time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`;
-  // const sql = `INSERT INTO fruits (name, description, fruit_picture_url, price, owner_id, isSold, list_time) VALUES ($2, $3, $4, $5, $6, $7, $8) RETURNING *;`;
+  // const sql = `INSERT INTO fruits (name, description, fruit_picture_url, price, owner_name, isSold, list_time) VALUES ($2, $3, $4, $5, $6, $7, $8) RETURNING *;`;
 
-  db.query(sql, [id, name, description, imageURL, price, ownerId, isSold, listTime])
+  db.query(sql, [id, name, description, imageURL, price, ownerID, isSold, listTime])
     .then(data => {
+      console.log(data.rows);
       res.redirect("/")
     })
     .catch(err => {
