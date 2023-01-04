22c45408036fe8e4da87f52e203b6f1d8a0cb7e3
Hilora Dewett <h.dewett@outlook.com>
2023-01-04 11:04:23 -0500

    add styling to moreInfoPopup in index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,19 @@
 module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+  },
+
   content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
   plugins: [
     require('tw-elements/dist/plugin')
