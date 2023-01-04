06c75313d87b1c6593e90fb20ce11a61a159233c
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-01-04 11:14:49 -0500

    merge changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'],
+  content: ['./src/**/*.{html,js,ejs}', './node_modules/tw-elements/dist/js/**/*.js'],
   plugins: [
     require('tw-elements/dist/plugin')
   ]
