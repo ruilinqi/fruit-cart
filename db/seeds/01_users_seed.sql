919b735d3178a7e60aae5e18df832d57f44ab708
Hilora Dewett <h.dewett@outlook.com>
2023-01-04 15:46:33 -0500

    update users and fruits seed files

--- db/seeds/01_users_seed.sql
+++ db/seeds/01_users_seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (name, password, email, phone, picture_url, isAdmin)
-VALUES ('Alice', 'alice2010', 'alice@wonderland.com', '123-123-1234', 'https://i.imgur.com/nlhLi3I.png', FALSE),
-('Batman', 'batman2008', 'batman@gotham.com', '777-888-9999', 'https://i.imgur.com/ilT4JDe.png', FALSE),
+VALUES ('Admin', '$2a$10$6XLIY4sdzKU23iNRcrLqm.EXr1vOgVr4k/IVbXqKHxbMLlEIOeAjS', 'admin@fruitmail.com', '123-123-1234', 'https://i.imgur.com/nlhLi3I.png', TRUE),
+('Batman', '$2a$10$J9Rl6JIoXRMGHha/gvvjEud5PfTj1QaLX6iKOPX6J5yelsLlwSMhK', 'batman@gotham.com', '777-888-9999', 'https://i.imgur.com/ilT4JDe.png', FALSE),
 ('Raylene', 'raylene2022', 'raylene@gmail.com', '647-123-4567', 'https://i.imgur.com/z5LNkkB.png', TRUE),
 ('Hilora', 'hilora2022', 'hilora@gmail.com', '647-123-4567', 'https://i.imgur.com/lRUnDgU.png', TRUE),
 ('Juntao', 'juntao2022', 'juntao@gmail.com', '647-123-4567', 'https://i.imgur.com/5fUVPRP.png', TRUE);
