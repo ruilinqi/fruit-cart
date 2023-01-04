137717c650e64a8e7ce34236cdce2d785199acad
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-01-04 14:30:34 -0500

    update signup

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('../db/connection');
 const router  = express.Router();
-
+const bcrypt = require("bcryptjs");
 
 
 router.get("/signup", (req, res) => {
@@ -11,22 +11,23 @@ router.get("/signup", (req, res) => {
 });
 
 router.post("/signup", (req, res) => {
-  const id = req.body.id;
+
   const name = req.body.name;
-  const password = req.body.password;
+  const inputPassword = req.body.password;
+  const password = bcrypt.hashSync(inputPassword, 10);
   const email = req.body.email;
   const phone = req.body.phone;
   const pictureURL = "";
-  const isAdmin = false;
+  const isAdmin = true;
 
 
-  const sql = `INSERT INTO users (id, name, password, email, phone, picture_url, isAdmin) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`;
+  const sql = `INSERT INTO users (name, password, email, phone, picture_url, isAdmin) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`;
   // const sql = `INSERT INTO fruits (name, description, fruit_picture_url, price, owner_id, isSold, list_time) VALUES ($2, $3, $4, $5, $6, $7, $8) RETURNING *;`;
 
-  db.query(sql, [id, name, password, email, phone, pictureURL, isAdmin])
+  db.query(sql, [name, password, email, phone, pictureURL, isAdmin])
     .then(data => {
       console.log("User Data:", data);
-      res.redirect("/");
+      res.redirect("/login");
     })
     .catch(err => {
       res
