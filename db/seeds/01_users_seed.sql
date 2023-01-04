53de6bb5081dfb457bfa3757e24b6e9e2118f88e
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-01-04 14:31:48 -0500

    update user database

--- db/seeds/01_users_seed.sql
+++ db/seeds/01_users_seed.sql
@@ -1,9 +1,9 @@
-INSERT INTO users (id, name, password, email, phone, picture_url, isAdmin)
-VALUES (1, 'Alice', 'alice2010', 'alice@wonderland.com', '123-123-1234', 'https://i.imgur.com/nlhLi3I.png', FALSE),
-(2, 'Batman', 'batman2008', 'batman@gotham.com', '777-888-9999', 'https://i.imgur.com/ilT4JDe.png', FALSE),
-(3, 'Raylene', 'raylene2022', 'raylene@gmail.com', '647-123-4567', 'https://i.imgur.com/z5LNkkB.png', TRUE),
-(4, 'Hilora', 'hilora2022', 'hilora@gmail.com', '647-123-4567', 'https://i.imgur.com/lRUnDgU.png', TRUE),
-(5, 'Juntao', 'juntao2022', 'juntao@gmail.com', '647-123-4567', 'https://i.imgur.com/5fUVPRP.png', TRUE);
+INSERT INTO users (name, password, email, phone, picture_url, isAdmin)
+VALUES ('Alice', 'alice2010', 'alice@wonderland.com', '123-123-1234', 'https://i.imgur.com/nlhLi3I.png', FALSE),
+('Batman', 'batman2008', 'batman@gotham.com', '777-888-9999', 'https://i.imgur.com/ilT4JDe.png', FALSE),
+('Raylene', 'raylene2022', 'raylene@gmail.com', '647-123-4567', 'https://i.imgur.com/z5LNkkB.png', TRUE),
+('Hilora', 'hilora2022', 'hilora@gmail.com', '647-123-4567', 'https://i.imgur.com/lRUnDgU.png', TRUE),
+('Juntao', 'juntao2022', 'juntao@gmail.com', '647-123-4567', 'https://i.imgur.com/5fUVPRP.png', TRUE);
 
 
 
