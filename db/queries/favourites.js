81065b3bfd50bec8350bebc8e5ff1a575c3ff6d2
ruilinqi <ruilinqi7@gmail.com>
2023-01-05 23:16:46 +0000

    Updated favourites and shopping cart pages

--- db/queries/favourites.js
+++ db/queries/favourites.js
@@ -1,7 +1,8 @@
 const db = require('../connection');
 
 const getFavourites = (user_id) => {
-  return db.query(`SELECT fruits.id, fruits.name as fruit_name, fruits.price, users.id, users.name as user_name, users.email FROM favourites
+  return db.query(`SELECT fruits.id, fruits.name as fruit_name, fruits.price, fruits.fruit_picture_url,
+   users.id, users.name as user_name, users.email FROM favourites
   JOIN users ON users.id = favourites.user_id
   JOIN fruits ON fruits.id = favourites.fruit_id
   WHERE users.id = $1`, [user_id])
