84780abce6baf0e32f150659a1225b38d12d6a6c
ruilinqi <ruilinqi7@gmail.com>
2023-01-06 00:45:49 +0000

    Updated shopping list

--- db/queries/shopping_list.js
+++ db/queries/shopping_list.js
@@ -3,7 +3,8 @@ const db = require('../connection');
 const getShoppingItems = (user_id) => {
   return db.query(`SELECT fruits.id as fruit_id, fruits.name as fruit_name,
   fruits.price, fruits.fruit_picture_url,
-  users.id, users.name as user_name, users.email FROM shopping_list
+  users.id, users.name as user_name, users.email, shopping_list.id as shop_id
+  FROM shopping_list
   JOIN users ON users.id = shopping_list.user_id
   JOIN fruits ON fruits.id = shopping_list.fruit_id
   WHERE users.id = $1`, [user_id])
